0212bb2b0b3b02e661067fcd21cfab131c7550a4
noewibepsi <noe.wibaut@ecoles-epsi.net>
2025-09-25 12:46:48 +0200

    Update account.repository.js

---
+++ src/modules/banking/account/account.repository.js
@@ -0,0 +1,17 @@
+// account.repository.js
+
+export async function createAccountInRepository(data) {
+  throw new Error("Not implemented: createAccountInRepository");
+}
+
+export async function getAccountsFromRepository(userId) {
+  throw new Error("Not implemented: getAccountsFromRepository");
+}
+
+export async function deleteAccountInRepository(userId, accountId) {
+  throw new Error("Not implemented: deleteAccountInRepository");
+}
+
+export async function patchAccount(accountId, delta) {
+  throw new Error("Not implemented: patchAccount");
+}
